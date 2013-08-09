bd84bd5c51209237a0b5e1b044f03dba0262414e
Edric Navarro <digitalcomputer2142@gmail.com>
2013-08-09 12:19:15 -0430

    add list of credits

--- plg_content_bootmarkaccess/bootmarkaccess.php
+++ plg_content_bootmarkaccess/bootmarkaccess.php
@@ -1,7 +1,8 @@
 <?php defined('_JEXEC') or die;
 /**
- * @copyright   Copyright (C) 2013 mktgexperts.com. All rights reserved.
+ * @copyright   Copyright (C) 2013 bootmark-framework.com. All rights reserved.
  * @license     GNU General Public License version 2 or later; see http://www.gnu.org/licenses/gpl-2.0.html
+ * @credits       Check credits.html included in this package or repository for a full list of credits
  */
 
 
